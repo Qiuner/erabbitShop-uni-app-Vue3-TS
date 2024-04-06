958a5bcbe8082f7a238e331b582c95a72b3a4dfd
Qiu <wdst3635@163.com>
2024-04-06 15:54:58 +0800

    前台分类数据类型渲染

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,24 +1,38 @@
 <script setup lang="ts">
-import { getHomeBannerAPI } from '@/services/home'
+import { getHomeBannerAPI, getHomeCategoryAPI } from '@/services/home'
 import CoustomNavbar from './components/CustomNavbar.vue'
 import { ref } from 'vue'
 import { onLoad } from '@dcloudio/uni-app'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
 
+// 获取轮播图数据
 const bannerList = ref<BannerItem[]>([])
 const getHomeBannerDate = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
+const categoryList = ref<CategoryItem[]>([])
+const getHomeCategoryDate = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
 onLoad(() => {
   getHomeBannerDate()
+  getHomeCategoryDate()
 })
 </script>
 
 <template>
   <CoustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel :list="categoryList" />
   <view class="index">index </view>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+// 修改小程序的颜色 类似html5中为body指定颜色
+page {
+  background-color: #f7f7f7;
+}
+</style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,6 +1,7 @@
 // 存放路径: src/services/home.ts
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem } from '@/types/home'
 import { http } from '@/utils/http'
+
 /**
  * 首页-广告区域-小程序
  * @param distributionSite 广告区域展示位置（投放位置 投放位置，1为首页，2为分类商品页） 默认是1
@@ -12,5 +13,15 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
+  })
+}
+// services/home.ts
+/**
+ * 首页-前台分类-小程序
+ */
+export const getHomeCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli',
   })
 }
