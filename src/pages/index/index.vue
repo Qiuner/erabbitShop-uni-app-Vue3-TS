d297093cc36bc29f8babb788e2bf5e1eb706b38b
Qiu <wdst3635@163.com>
2024-04-06 15:18:42 +0800

    轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
 import CoustomNavbar from './components/CustomNavbar.vue'
+import { ref } from 'vue'
+import { onLoad } from '@dcloudio/uni-app'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerDate = async () => {
+  const res = await getHomeBannerAPI()
+  bannerList.value = res.result
+}
+onLoad(() => {
+  getHomeBannerDate()
+})
 </script>
 
 <template>
   <CoustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
   <view class="index">index </view>
 </template>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
