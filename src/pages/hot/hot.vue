b2e7684da9bcc2e76ddd8f52dc369bed592a8e90
Qiu <wdst3635@163.com>
2024-04-07 16:34:05 +0800

    feat:热门推荐渲染和tab交互思路

--- src/pages/hot/hot.vue
+++ src/pages/hot/hot.vue
@@ -1,8 +1,9 @@
 // src/pages/hot/hot.vue
 <script setup lang="ts">
 import { getHotRecommendAPI } from '@/services/hot'
-import { onLoad } from '@dcloudio/uni-app';
-
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { SubTypeItem } from '@/types/hot'
 // 热门推荐页 标题和url
 const hotMap = [
   { type: '1', title: '特惠推荐', url: '/hot/preference' },
@@ -14,15 +15,22 @@ const hotMap = [
 const query = defineProps<{
   type: string
 }>()
-console.log(query)
 const currHot = hotMap.find((v) => v.type === query.type)
 // 动态设置标题 这里有跳转才会来到这里 所以不可能出现空值
 uni.setNavigationBarTitle({ title: currHot!.title })
+// 推荐封面图
+const bannerPicture = ref('')
+// 推荐选项
+const subTypes = ref<SubTypeItem[]>([])
+
+// 高亮的下标
+const activeIndex = ref(0)
 
 // 获取热门推荐数据
 const getgetHotRecommendData = async () => {
   const res = await getHotRecommendAPI(currHot!.url)
-  console.log(res)
+  bannerPicture.value = res.result.bannerPicture
+  subTypes.value = res.result.subTypes
 }
 
 onLoad(() => {
@@ -34,34 +42,41 @@ onLoad(() => {
   <view class="viewport">
     <!-- 推荐封面图 -->
     <view class="cover">
-      <image
-        src="http://yjy-xiaotuxian-dev.oss-cn-beijing.aliyuncs.com/picture/2021-05-20/84abb5b1-8344-49ae-afc1-9cb932f3d593.jpg"
-      >
-      </image>
+      <image :src="bannerPicture"> </image>
     </view>
     <!-- 推荐选项 -->
     <view class="tabs">
-      <text class="text active">抢先尝鲜</text>
+      <text
+        v-for="(item, index) in subTypes"
+        :key="item.id"
+        class="text"
+        :class="{ active: index === activeIndex }"
+        @tap="activeIndex = index"
+        >{{ item.title }}</text
+      >
       <text class="text">新品预告</text>
     </view>
     <!-- 推荐列表 -->
-    <scroll-view scroll-y class="scroll-view">
+    <scroll-view
+      v-for="(item, index) in subTypes"
+      :key="item.id"
+      v-show="activeIndex === index"
+      scroll-y
+      class="scroll-view"
+    >
       <view class="goods">
         <navigator
           hover-class="none"
           class="navigator"
-          v-for="goods in 10"
-          :key="goods"
-          :url="`/pages/goods/goods?id=`"
+          v-for="goods in item.goodsItems.items"
+          :key="goods.id"
+          :url="`/pages/goods/goods?id=${goods.id}`"
         >
-          <image
-            class="thumb"
-            src="https://yanxuan-item.nosdn.127.net/5e7864647286c7447eeee7f0025f8c11.png"
-          ></image>
-          <view class="name ellipsis">不含酒精，使用安心爽肤清洁湿巾</view>
+          <image class="thumb" :src="goods.picture"></image>
+          <view class="name ellipsis">{{ goods.name }}</view>
           <view class="price">
             <text class="symbol">¥</text>
-            <text class="number">29.90</text>
+            <text class="number">{{ goods.price }}</text>
           </view>
         </navigator>
       </view>
