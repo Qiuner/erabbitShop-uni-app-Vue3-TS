4c312064ea7c61472e2dda15948a5541701cfe72
Qiu <wdst3635@163.com>
2024-04-08 17:23:14 +0800

    P42商品详情 服务接口响应很慢 先提交了 后面再改

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -1,9 +1,11 @@
-// src/pages/goods/goods.vue
 <script setup lang="ts">
 import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
+
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 
@@ -12,15 +14,46 @@ const query = defineProps<{
   id: string
 }>()
 
+// 获取商品详情信息
 const goods = ref<GoodsResult>()
 const getGoodsByIdData = async () => {
   const res = await getGoodsByIdAPI(query.id)
   goods.value = res.result
 }
 
+// 页面加载
 onLoad(() => {
   getGoodsByIdData()
 })
+
+// 轮播图变化时
+const currentIndex = ref(0)
+const onChange: UniHelper.SwiperOnChange = (ev) => {
+  currentIndex.value = ev.detail.current
+}
+
+// 点击图片时
+const onTapImage = (url: string) => {
+  // 大图预览
+  uni.previewImage({
+    current: url,
+    urls: goods.value!.mainPictures,
+  })
+}
+
+// uni-ui 弹出层组件 ref
+const popup = ref<{
+  open: (type?: UniHelper.UniPopupType) => void
+  close: () => void
+}>()
+
+// 弹出层条件渲染
+const popupName = ref<'address' | 'service'>()
+const openPopup = (name: typeof popupName.value) => {
+  // 修改弹出层名称
+  popupName.value = name
+  popup.value?.open()
+}
 </script>
 
 <template>
@@ -29,15 +62,15 @@ onLoad(() => {
     <view class="goods">
       <!-- 商品主图 -->
       <view class="preview">
-        <swiper circular>
+        <swiper @change="onChange" circular>
           <swiper-item v-for="item in goods?.mainPictures" :key="item">
-            <image mode="aspectFill" :src="item" />
+            <image @tap="onTapImage(item)" mode="aspectFill" :src="item" />
           </swiper-item>
         </swiper>
         <view class="indicator">
-          <text class="current">1</text>
+          <text class="current">{{ currentIndex + 1 }}</text>
           <text class="split">/</text>
-          <text class="total">5</text>
+          <text class="total">{{ goods?.mainPictures.length }}</text>
         </view>
       </view>
 
@@ -47,7 +80,7 @@ onLoad(() => {
           <text class="symbol">¥</text>
           <text class="number">{{ goods?.price }}</text>
         </view>
-        <view class="name ellipsis">{{ goods?.name }} </view>
+        <view class="name ellipsis">{{ goods?.name }}</view>
         <view class="desc"> {{ goods?.desc }} </view>
       </view>
 
@@ -57,11 +90,11 @@ onLoad(() => {
           <text class="label">选择</text>
           <text class="text ellipsis"> 请选择商品规格 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis"> 请选择收获地址 </text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis"> 无忧退 快速退款 免费包邮 </text>
         </view>
@@ -76,19 +109,17 @@ onLoad(() => {
       <view class="content">
         <view class="properties">
           <!-- 属性详情 -->
-          <view class="item" v-for="item in goods?.details.properties" :key="item.value">
+          <view class="item" v-for="item in goods?.details.properties" :key="item.name">
             <text class="label">{{ item.name }}</text>
             <text class="value">{{ item.value }}</text>
           </view>
         </view>
         <!-- 图片详情 -->
         <image
+          v-for="item in goods?.details.pictures"
+          :key="item"
           mode="widthFix"
-          src="https://yanxuan-item.nosdn.127.net/a8d266886d31f6eb0d7333c815769305.jpg"
-        ></image>
-        <image
-          mode="widthFix"
-          src="https://yanxuan-item.nosdn.127.net/a9bee1cb53d72e6cdcda210071cbd46a.jpg"
+          :src="item"
         ></image>
       </view>
     </view>
@@ -100,21 +131,17 @@ onLoad(() => {
       </view>
       <view class="content">
         <navigator
-          v-for="item in 4"
-          :key="item"
+          v-for="item in goods?.similarProducts"
+          :key="item.id"
           class="goods"
           hover-class="none"
-          :url="`/pages/goods/goods?id=`"
+          :url="`/pages/goods/goods?id=${item.id}`"
         >
-          <image
-            class="image"
-            mode="aspectFill"
-            src="https://yanxuan-item.nosdn.127.net/e0cea368f41da1587b3b7fc523f169d7.png"
-          ></image>
-          <view class="name ellipsis">简约山形纹全棉提花毛巾</view>
+          <image class="image" mode="aspectFill" :src="item.picture"></image>
+          <view class="name ellipsis">{{ item.name }}</view>
           <view class="price">
             <text class="symbol">¥</text>
-            <text class="number">18.50</text>
+            <text class="number">{{ item.price }}</text>
           </view>
         </navigator>
       </view>
@@ -128,15 +155,19 @@ onLoad(() => {
       <button class="icons-button" open-type="contact">
         <text class="icon-handset"></text>客服
       </button>
-      <navigator class="icons-button" url="/pages/cart/cart" open-type="switchTab">
-        <text class="icon-cart"></text>购物车
-      </navigator>
+      <navigator class="icons-button"><text class="icon-cart"></text>购物车</navigator>
     </view>
     <view class="buttons">
       <view class="addcart"> 加入购物车 </view>
-      <view class="buynow"> 立即购买 </view>
+      <view class="payment"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层 -->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close()" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close()" />
+  </uni-popup>
 </template>
 
 <style lang="scss">
@@ -195,10 +226,6 @@ page {
   .preview {
     height: 750rpx;
     position: relative;
-    .image {
-      width: 750rpx;
-      height: 750rpx;
-    }
     .indicator {
       height: 40rpx;
       padding: 0 24rpx;
@@ -290,9 +317,6 @@ page {
   padding-left: 20rpx;
   .content {
     margin-left: -20rpx;
-    .image {
-      width: 100%;
-    }
   }
   .properties {
     padding: 0 20rpx;
@@ -316,20 +340,21 @@ page {
 
 /* 同类推荐 */
 .similar {
+  padding-left: 20rpx;
   .content {
-    padding: 0 20rpx 200rpx;
+    padding: 0 20rpx 20rpx;
+    margin-left: -20rpx;
     background-color: #f4f4f4;
-    display: flex;
-    flex-wrap: wrap;
-    .goods {
-      width: 340rpx;
+    overflow: hidden;
+    navigator {
+      width: 345rpx;
       padding: 24rpx 20rpx 20rpx;
-      margin: 20rpx 7rpx;
+      margin: 20rpx 20rpx 0 0;
       border-radius: 10rpx;
       background-color: #fff;
+      float: left;
     }
     .image {
-      width: 300rpx;
       height: 260rpx;
     }
     .name {
@@ -357,14 +382,9 @@ page {
 
 /* 底部工具栏 */
 .toolbar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 1;
   background-color: #fff;
   height: 100rpx;
-  padding: 0 20rpx var(--window-bottom);
+  padding: 0 20rpx;
   border-top: 1rpx solid #eaeaea;
   display: flex;
   justify-content: space-between;
@@ -383,7 +403,6 @@ page {
     .addcart {
       background-color: #ffa868;
     }
-    .buynow,
     .payment {
       background-color: #27ba9b;
       margin-left: 20rpx;
@@ -404,9 +423,6 @@ page {
       font-size: 20rpx;
       color: #333;
       background-color: #fff;
-      &::after {
-        border: none;
-      }
     }
     text {
       display: block;
