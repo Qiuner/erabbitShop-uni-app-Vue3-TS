ccdf5b772ec2dee7f393b57e9145341f0c78c6d6
Qiu <wdst3635@163.com>
2024-04-12 10:59:49 +0800

    feat:订单支付页面

--- src/pagesOrder/detail/detail.vue
+++ src/pagesOrder/detail/detail.vue
@@ -5,6 +5,7 @@ import { onLoad, onReady } from '@dcloudio/uni-app'
 import { getMemberOrderByIdAPI, getRepurchaseOrderByIdAPI } from '@/services/order'
 import type { OrderResult } from '@/types/order'
 import { OrderState, orderStateList } from '@/services/constants'
+import { getPayMockAPI, getPayWxPayMiniPayAPI } from '@/services/pay'
 // 获取屏幕边界到安全区域距离
 const { safeAreaInsets } = uni.getSystemInfoSync()
 // 猜你喜欢
@@ -87,6 +88,20 @@ const onTimeup = () => {
   // 修改订单状态为已取消
   order.value!.orderState = OrderState.YiQuXiao
 }
+// 订单支付
+const onOrderPay = async () => {
+  // 通过环境变量区分开发环境
+  if (import.meta.env.DEV) {
+    // 开发环境：模拟支付，修改订单状态为已支付
+    await getPayMockAPI({ orderId: query.id })
+  } else {
+    // 生产环境：获取支付参数 + 发起微信支付
+    const res = await getPayWxPayMiniPayAPI({ orderId: query.id })
+    await wx.requestPayment(res.result)
+  }
+  // 关闭当前页，再跳转支付结果页
+  uni.redirectTo({ url: `/pagesOrder/payment/payment?id=${query.id}` })
+}
 </script>
 
 <template>
@@ -123,7 +138,7 @@ const onTimeup = () => {
               @timeup="onTimeup"
             />
           </view>
-          <view class="button">去支付</view>
+          <view class="button" @tap="onOrderPay">去支付</view>
         </template>
         <!-- 其他订单状态:展示再次购买按钮 -->
         <template v-else>
@@ -710,3 +725,4 @@ page {
   }
 }
 </style>
+import { getPayMockAPI, getPayWxPayMiniPayAPI } from '@/services/pay'
